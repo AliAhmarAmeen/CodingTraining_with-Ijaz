4ca3203f474f3291d87023e3971e1c32261640bc
Ali Ahmar <ahmartech969@gmail.com>
2025-01-16 10:23:54 +0500

    Shifted to PC from Chromebook

---
+++ Javascrip/js/script.js
@@ -0,0 +1,59 @@
+console.log("Script.js file");
+
+// --- Leetcode ---
+
+// data types ------
+
+
+// String
+// "Ijaz Qadri 343455 ^ & ? "HTML" dsgdf" // wrong
+"Ijaz 'Qadri' 343455 ^ & ? 'HTML' ds gdf" // right
+
+
+
+""
+''
+``
+
+
+
+'Quaid-e-Azam is "founder" of "PAK"'
+"mirza01it@gmail.com"
+"dsgfdghfsh2@gmail.com"
+
+// Maybe in python
+// 'a'
+// "asdfsadf"
+// Maybe in python
+
+
+// "PAK"
+
+
+
+
+
+
+
+
+
+
+
+// Number
+3456
+
+// boolean
+
+// Array
+
+// Object
+
+// undefined
+
+// null
+
+// Set
+
+
+// BigInt 
+
