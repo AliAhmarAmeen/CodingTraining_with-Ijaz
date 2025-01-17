3be6e5325bc5194bd82e1238cff79b5ef46c9d28
Ali Ahmar <ahmartech969@gmail.com>
2025-01-17 22:07:11 +0500

    variables, data types, print statements

---
+++ Javascript/js/script.js
@@ -0,0 +1,153 @@
+
+// console.log("Script.js file");
+
+// last queries, AD's, node js, python --
+
+
+// 1. data types
+// 2. operators
+// 3. logs & alerts
+
+
+
+
+
+
+
+
+
+
+
+// ==================ROUGH==================== //
+
+// --- Leetcode ---
+
+// data types ------
+
+// String ==
+// "Ijaz Qadri 343455 ^ & ? "HTML" dsgdf" // wrong
+// "Ijaz 'Qadri' 343455 ^ & ? 'HTML' ds gdf" // right
+
+// ""
+// ''
+// ``
+
+// 'Quaid-e-Azam is "founder" of "PAK"'
+// "mirza01it@gmail.com"
+// "dsgfdghfsh2@gmail.com"
+
+// Maybe in python
+// 'a'
+// "asdfsadf"
+// Maybe in python
+// "PAK"
+
+// Number
+// null
+// undefined
+// true, false
+// string
+// 
+
+// Variable Declaring methods ++
+
+// var **
+// ------------
+
+var firstName = "Ijaz Qadri";
+var age = 22;
+var ageString = "22";
+
+var muslim = true;
+var muslimFalse = false;
+
+var gender; // undefined
+var noValue = null;
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+firstName = "Qadri"; //reassign
+
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// let ***
+// --------
+let fatherName = "xyz";
+
+console.log(fatherName, " : Father Name");
+fatherName = "yyy";
+
+console.log(fatherName, " : Father Name changed");
+
+
+// const (no value change) ***
+
+// const fullName = "Ijaz Hussain";
+// fullName = "Ijaz";
+
+// console.log(fullName);
+
+// 3. Logs and Alerts **
+// ----------------
+
+// Outputs prints in console ===
+
+// console.log(firstName);
+// console.error(noValue);
+// console.warn("Warning");
+
+// var heading = document.getElementById("heading-4");
+// heading.textContent = firstName;
+// console.log(heading);
+// console.log(heading.textContent);
+
+// console.log("dfasdfasdfsd");
+
+// let alertMessage = alert("Your name is Ijaz");
+
+// let confirmMessage = confirm("Your are agree?");
+// console.log(confirmMessage);
+
+let ageFromPrompt = prompt("What is your Age?");
+console.log(ageFromPrompt);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Number
+// 3456
+
+// boolean
+
+// Array
+
+// Object
+
+// undefined
+
+// null
+
+// Set
+
+
+// BigInt 
+
